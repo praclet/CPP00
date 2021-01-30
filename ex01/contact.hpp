d35ba14c72905858112163c5c3d6cf9bb79c0e03
praclet <76652978+praclet@users.noreply.github.com>
2021-01-30 18:49:49 +0100

    Create contact.hpp

---
+++ ex01/contact.hpp
@@ -0,0 +1,17 @@
+#include <string>
+
+class Contact
+{
+	public:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string login;
+		std::string postal_address;
+		std::string email_address;
+		std::string phone_number;
+		std::string birthday date;
+		std::string favorite_meal;
+		std::string underwear_color;
+		std::string darkest_secret;
+};
