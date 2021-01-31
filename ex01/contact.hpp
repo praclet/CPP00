4b46121244dd85d76d3e02f27772c1409395cf39
Philippe Raclet <praclet.tel@gmail.com>
2021-01-31 09:18:04 +0100

    Saving files

--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -1,3 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/31 08:43:31 by praclet           #+#    #+#             */
+/*   Updated: 2021/01/31 08:57:38 by praclet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
 #include <string>
 
 class Contact
@@ -10,8 +25,10 @@ class Contact
 		std::string postal_address;
 		std::string email_address;
 		std::string phone_number;
-		std::string birthday date;
+		std::string birthday_date;
 		std::string favorite_meal;
 		std::string underwear_color;
 		std::string darkest_secret;
 };
+
+#endif
--- ex01/phone_book.hpp
+++ ex01/phone_book.hpp
@@ -6,8 +6,32 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 08:37:33 by praclet           #+#    #+#             */
-/*   Updated: 2021/01/31 08:37:36 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/01/31 09:16:25 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
 
+# include "contact.hpp"
+
+class Phone_book.hpp
+{
+	public:
+				Phone_book(void) : nb_contacts(0)
+				{};
+		int		is_full()
+				{return (nb_contacts < 8);};
+		void	add(Contact contact)
+				{phone_book[nb_contact++] = contact;};
+		contact& operator[](std::size_t i)
+				{return (phone_book[i]);};
+		const contact& operator[](std::size_t i) const
+				{return (phone_book[i]);};
+
+	private:
+		int		nb_contacts;
+		Contact	phone_book[8];
+};
+
+#endif
