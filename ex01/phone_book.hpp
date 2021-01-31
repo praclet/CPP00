358cafe3883d7a9d786968e031d10696c6a5da30
Philippe Raclet <praclet.tel@gmail.com>
2021-01-31 08:40:30 +0100

    Saving files

--- ex01/phone_book.hpp
+++ ex01/phone_book.hpp
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phone_book.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/29 14:57:03 by praclet           #+#    #+#             */
-/*   Updated: 2021/01/31 08:31:54 by praclet          ###   ########lyon.fr   */
+/*   Created: 2021/01/31 08:37:33 by praclet           #+#    #+#             */
+/*   Updated: 2021/01/31 08:37:36 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cctype> 
 
-int main(int argc, char *argv[])
-{
-	int		i;
-	char	*str;
-
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-		for (i = 1;i < argc;i++)
-		{
-			str = argv[i];
-			while (*str)
-				std::cout << (char)toupper(*str++);
-		}
-	std::cout << std::endl;
-	return (0);
-}
