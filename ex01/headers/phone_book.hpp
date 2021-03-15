6daed88ba092959eb4127779f91433673a48b55e
Philippe Raclet <praclet.tel@gmail.com>
2021-03-15 10:44:27 +0100

    Saving files

--- ex01/headers/phone_book.hpp
+++ ex01/headers/phone_book.hpp
@@ -6,7 +6,7 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/31 08:37:33 by praclet           #+#    #+#             */
-/*   Updated: 2021/03/08 13:53:46 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2021/03/15 10:31:04 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,11 @@
 class Phone_book
 {
 	public:
-		Phone_book(void) : nb_contacts(0)
-			{
-			};
-		int	is_full() const
-			{
-				return (nb_contacts >= sizeof(phone_book)/sizeof(phone_book[0]));
-			};
-		void	add(Contact *contact)
-			{
-				if (!this->is_full())
-					phone_book[nb_contacts++] = *contact;
-			};
-		Contact*	getContact(std::size_t i)
-			{
-				return (i < nb_contacts ? phone_book + i : NULL);
-			};
-		std::size_t	length(void) const
-			{
-				return (nb_contacts);
-			};
+					Phone_book(void);
+		int			is_full() const;
+		void		add(Contact *contact);
+		Contact*	getContact(std::size_t i) const;
+		std::size_t	length(void) const;
 	private:
 		std::size_t		nb_contacts;
 		Contact			phone_book[8];
---
+++ ex01/srcs/phone_book.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phone_book.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/15 10:26:16 by praclet           #+#    #+#             */
+/*   Updated: 2021/03/15 10:31:00 by praclet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phone_book.hpp"
+#include "contact.hpp"
+
+Phone_book::Phone_book(void) : nb_contacts(0)
+{
+};
+
+int	Phone_book::is_full() const
+{
+	return (nb_contacts >= sizeof(phone_book)/sizeof(phone_book[0]));
+};
+
+void	Phone_book::add(Contact *contact)
+{
+	if (!this->is_full())
+		phone_book[nb_contacts++] = *contact;
+};
+
+Contact*	Phone_book::getContact(std::size_t i) const
+{
+	return (i < nb_contacts ? phone_book + i : NULL);
+};
+
+std::size_t	Phone_book::length(void) const
+{
+	return (nb_contacts);
+};
